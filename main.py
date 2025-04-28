4e7c8f0c0655aa969cfb0e0417dd7949c212f137
medinapdr <112674317+medinapdr@users.noreply.github.com>
2025-04-28 20:23:30 -0300

    4. Testando TestCase

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from src.xunit.test_result import TestResult
-from tests.my_test import MyTest
+from tests.test_case_test import TestCaseTest
 
 if __name__ == "__main__":
     result = TestResult()
 
-    test = MyTest('test_a')
+    test = TestCaseTest('test_result_success_run')
     test.run(result)
 
-    test = MyTest('test_b')
+    test = TestCaseTest('test_result_failure_run')
     test.run(result)
 
-    test = MyTest('test_c')
+    test = TestCaseTest('test_result_error_run')
+    test.run(result)
+
+    test = TestCaseTest('test_result_multiple_run')
+    test.run(result)
+
+    test = TestCaseTest('test_was_set_up')
+    test.run(result)
+
+    test = TestCaseTest('test_was_run')
+    test.run(result)
+
+    test = TestCaseTest('test_was_tear_down')
+    test.run(result)
+
+    test = TestCaseTest('test_template_method')
     test.run(result)
 
     print(result.summary())
---
+++ src/xunit/test_stub.py
@@ -0,0 +1,11 @@
+from src.xunit.test_case import TestCase
+
+class TestStub(TestCase):
+    def test_success(self):
+        assert True
+
+    def test_failure(self):
+        assert False
+
+    def test_error(self):
+        raise Exception
