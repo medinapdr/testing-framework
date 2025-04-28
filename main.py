9fa7b0525060f24a068b9958ac4e8b5fb55931d9
medinapdr <112674317+medinapdr@users.noreply.github.com>
2025-04-28 20:47:05 -0300

    6. Classes TestLoader e TestRunner

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
+from src.xunit.test_loader import TestLoader
 from src.xunit.test_result import TestResult
+from src.xunit.test_runner import TestRunner
 from src.xunit.test_suite import TestSuite
 from tests.test_case_test import TestCaseTest
+from tests.test_loader_test import TestLoaderTest
 from tests.test_suite_test import TestSuiteTest
 
 if __name__ == "__main__":
-    result = TestResult()
-    suite = TestSuite()
+    loader = TestLoader()
+    suite = loader.make_suite(TestLoaderTest)
 
-    suite.add_test(TestCaseTest('test_result_success_run'))
-    suite.add_test(TestCaseTest('test_result_failure_run'))
-    suite.add_test(TestCaseTest('test_result_error_run'))
-    suite.add_test(TestCaseTest('test_result_multiple_run'))
-    suite.add_test(TestCaseTest('test_was_set_up'))
-    suite.add_test(TestCaseTest('test_was_run'))
-    suite.add_test(TestCaseTest('test_was_tear_down'))
-    suite.add_test(TestCaseTest('test_template_method'))
-
-    suite.add_test(TestSuiteTest('test_suite_size'))
-    suite.add_test(TestSuiteTest('test_suite_success_run'))
-    suite.add_test(TestSuiteTest('test_suite_multiple_run'))
-
-    suite.run(result)
-    print(result.summary())
+    runner = TestRunner()
+    runner.run(suite)
