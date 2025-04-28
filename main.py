15698e33cd9f52b67b424cbc9ccb98ae73bbf252
medinapdr <112674317+medinapdr@users.noreply.github.com>
2025-04-28 20:16:40 -0300

    3. Classe TestResult

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+from src.xunit.test_result import TestResult
 from tests.my_test import MyTest
 
 if __name__ == "__main__":
+    result = TestResult()
+
     test = MyTest('test_a')
-    test.run()
+    test.run(result)
 
     test = MyTest('test_b')
-    test.run()
+    test.run(result)
 
     test = MyTest('test_c')
-    test.run()
+    test.run(result)
+
+    print(result.summary())
